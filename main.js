8f85e178a53ea249e6647b949bb6bad20478a894
Trang Lê Thị Thu <19522382@ms.uit.edu.vn>
2023-05-13 15:55:10 +0700

    feat: validate date and month

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const dateEle = document.getElementById("day");
+const dayEle = document.getElementById("day");
 const monthEle = document.getElementById("month");
 const yearEle = document.getElementById("year");
 const form = document.querySelector("form");
+
+const dateCurrent = new Date();
 
 //show input error message
 const showError = (input, message) => {
   // get element contain input and style for error
   const formControl = input.parentElement;
 
-  const labelDay = formControl.querySelector("label");
-  labelDay.style.color = "hsl(0, 100%, 67%)";
+  const labels = document.getElementsByClassName("label-input");
+  labels.className = "label-input error";
 
   input.className = "input input-error";
 
@@ -21,23 +23,46 @@ const showError = (input, message) => {
 //handle Input
 
 const handleInput = () => {
-  const dateValue = dateEle.value.trim();
+  const dayValue = dayEle.value.trim();
   const monthValue = monthEle.value.trim();
   const yearValue = yearEle.value.trim();
 
+  const listOfDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
   //check date
-  if (dateValue === "") {
-    showError(dateEle, "This field is required");
+  if (dayValue === "") {
+    showError(dayEle, "This field is required");
+  } else if (dayValue > listOfDays) {
+    showError(dayEle, "Must be a valid date");
+  } else if (monthValue === "1" || monthValue > "2") {
+    if (dayValue > listOfDays[monthValue - 1]) {
+      showError(dayEle, "Must be a valid date");
+    }
+  } else if (monthValue === "2") {
+    let leapYear = false;
+    if ((!(yearValue % 4) && yearValue % 100) || !(yearValue % 400)) {
+      leapYear = true;
+    }
+
+    if (leapYear && dayValue > 29) {
+      showError(dayEle, "Must be a valid date");
+    } else if (!leapYear && dayValue >= 29) {
+      showError(dayEle, "Must be a valid date");
+    }
   }
 
   //check month
   if (monthValue === "") {
     showError(monthEle, "This field is required");
+  } else if (monthValue >= 13) {
+    showError(monthEle, "Must be a valid month");
   }
 
   //check year
   if (yearValue === "") {
     showError(yearEle, "This field is required");
+  } else if (yearValue > dateCurrent.getFullYear()) {
+    showError(yearEle, "Must be in the past");
   }
 };
 
